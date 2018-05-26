0cc4fb494c083d6585ec284639f8b886d1fd7b3d
Lucas McDaniel <lmcdaniel@myemma.com>
2018-05-25 22:44:26 -0500

    Changed most things with my personal details

--- content/meta/config.js
+++ content/meta/config.js
@@ -1,28 +1,28 @@
 module.exports = {
-  siteTitle: "HeroBlog - a GatsbyJS starter", // <title>
-  shortSiteTitle: "HeroBlog - GatsbyJS starter", // <title> ending for posts and pages
-  siteDescription: "HeroBlog is a GatsbyJS starter.",
-  siteUrl: "https://gatsby-starter-hero-blog.greglobinski.com",
+  siteTitle: "Life in code - A scripted journey", // <title>
+  shortSiteTitle: "Life in code", // <title> ending for posts and pages
+  siteDescription: "Rubber ducking is next to godliness",
+  siteUrl: "https://life-in-code.lucasmcdaniel.com",
   pathPrefix: "",
   siteImage: "preview.jpg",
   siteLanguage: "en",
   // author
-  authorName: "greg lobinski",
-  authorTwitterAccount: "greglobinski",
+  authorName: "Lucas McDaniel",
+  authorTwitterAccount: "ldmcdaniel109",
   // info
-  headerTitle: "greg lobinski",
-  headerSubTitle: "presents another one GatsbyJS starter",
+  headerTitle: "Lucas McDaniel",
+  headerSubTitle: "One step along the path",
   // manifest.json
-  manifestName: "HeroBlog - a GatsbyJS starter",
-  manifestShortName: "HeroBlog", // max 12 characters
+  manifestName: "Life in code - A scripted journey",
+  manifestShortName: "Life in code", // max 12 characters
   manifestStartUrl: "/index.html",
   manifestBackgroundColor: "white",
   manifestThemeColor: "#666",
   manifestDisplay: "standalone",
   // social
   authorSocialLinks: [
-    { name: "github", url: "https://github.com/greglobinski" },
-    { name: "twitter", url: "https://twitter.com/greglobinski" },
-    { name: "facebook", url: "http://facebook.com/greglobinski" }
+    { name: "github", url: "https://github.com/ldmcdaniel" },
+    { name: "twitter", url: "https://twitter.com/ldmcdaniel109" },
+    { name: "facebook", url: "http://facebook.com/ldmcdaniel" }
   ]
 };
--- content/parts/footnote.md
+++ content/parts/footnote.md
@@ -2,9 +2,8 @@
 title: footnote
 ---
 
-* it's a demo site of the [gatsby-starter-hero-blog](https://github.com/greglobinski/gatsby-starter-hero-blog)
-* built by [greg lobinski](https://www.greglobinski.com)
-* Check [Front-end web development with Greg](https://dev.greglobinski.com)
+* built by [Lucas McDaniel](https://www.lucasmcdaniel.com)
+* Check [Life in code with Lucas](https://life-in-code.lucasmcdaniel.com)
 * deliverd by [Netlify](https://www.netlify.com/)
 * photos by [unsplash.com](https://unsplash.com)
 * graphic by [pixabay.com](https://pixabay.com)
